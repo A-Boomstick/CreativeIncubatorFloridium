3b1de3d7d98df0b7c6c8a79541ac70a9e033d47a
Teddeakin <147064059+Teddeakin@users.noreply.github.com>
2026-03-18 10:11:07 +0000

    general update
    Cleaned up some the code and added some comments

--- Webapp/index.js
+++ Webapp/index.js
@@ -26,39 +26,6 @@ app.use(
   }),
 );
 
-// Routes
-// app.get("/register", (req, res) => {
-//   res.sendFile(path.join(__dirname, "views", "register.html"));
-// });
-
-// app.post("/register", async (req, res) => {
-//   try {
-//     console.log("REQ BODY:", req.body);
-
-//     const { username, password } = req.body;
-//     console.log("USERNAME:", username);
-//     console.log("PASSWORD:", password);
-
-//     const success = await users.addUser(username, password);
-//     console.log("ADD USER RESULT:", success);
-
-//     if (!success) {
-//       return res.send("User already exists");
-//     }
-
-//     res.send("User created!");
-//   } catch (err) {
-//     console.log("REGISTER ERROR:", err);
-//     res.status(500).send("Error creating user");
-//   }
-// });
-
-
-// Optional homepage test route
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
-
 // Start server
 app.listen(3000, () => {
   console.log("Listening on http://localhost:3000");
--- Webapp/public/main.js
+++ Webapp/public/main.js
@@ -1,4 +1,4 @@
-const testdata = {
+const testdata = { // temp database
     plant_1: {
         moisture: "High",
         SoilHealth: "Good",
@@ -13,11 +13,10 @@ const testdata = {
     }
 };
 
-const renderPlants = () => {
-    const mainContainer = document.getElementById("plant-list");
-    let output = "";
+const renderPlants = () => { // function to make the plant containers and push it to the html page
+    let output = ""; // HTML syntax goes in here
 
-    Object.entries(testdata).forEach(([key, plant]) => {
+    Object.entries(testdata).forEach(([key, plant]) => { // loops through the object
         output += `
             <section class="PlantContainer">
                 <img src="Assets/721a75ada244b9bd4b43b76c9a256412f598e1f7.jfif" alt="Plant Image">
@@ -32,8 +31,7 @@ const renderPlants = () => {
         `;
     });
 
-    document.getElementById("plant-list").innerHTML = output;
+    document.getElementById("plant-list").innerHTML = output; // Pushes the output to the front end
 };
 
-// Start the process
 renderPlants();
