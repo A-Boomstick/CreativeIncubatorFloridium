5f78420fe6c0b3ddb87b4d8292b8c12b3e529ded
A-Boomstick <ELLIOTT.SHAW22@bathspa.ac.uk>
2026-03-18 09:23:03 +0000

    Trefle Working
    Done a basic test to interact with the Trefle API

--- Webapp/index.js
+++ Webapp/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
-
 const app = express();
 
+const path = require('path');
+
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
 
-app.use(express.static("./public"));
+async function postExample(request, response) {
+    const data = await fetch("https://trefle.io/api/v1/plants?token=usr-polxuVTSQmE9cwoXrS0KcwyZaZ-_o27woxACGgIvZEI");
+    const json = await data.json();
+    console.log(json);
+    response.send(json);
+}
+
+app.get("/example", postExample);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'placeholder.html'));
+}); 
