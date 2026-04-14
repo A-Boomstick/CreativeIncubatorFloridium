c4e63e4c89c05c9ac704b3f73c7bdb4fb9cea6f3
oxxeys <147168632+oxxeys@users.noreply.github.com>
2026-04-14 16:59:45 +0100

    Added html to page!

--- PhysicalCode/esp32code/esp32code.ino
+++ PhysicalCode/esp32code/esp32code.ino
@@ -5,26 +5,87 @@
 #include <DNSServer.h>
 
 // web server on port 80
-WifiServer server(80);
+WebServer server(80);
 
 //setup dns server
 DNSServer dnsServer;
+const byte DNS_PORT = 53;
 
 // setup prefrences
 Preferences prefrences;
 
+TaskHandle_t POSTTask;
+
+
 String ssid;
 String password;
+bool shouldRestart = false;
+
+static const String htmlPage = R"===(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Floridium Set Up!</title>
+</head>
+<body style="display: flex; flex-direction: column; align-items:center;">
+    <h1 style="color: #5EAF5E; margin-bottom: 0px;">Floridium<span style="color: black;">,</span></h1>
+    <h2 style="margin-top: 0px;">A smart plant pot</h2>
+    <p>To connect your plant pot to the internet, we need to connect to your router!</p>
+    <p>Please enter your SSID (the name of your wifi that you normally connect to) and the password for it!</p>
+
+    <p>SSID:</p>
+    <input id="ssid">
+    <p>Password:</p>
+    <input id="password" type="password">
+    <button onclick="saveData()">Save</button>
+    <script>
+      function saveData(){
+        let ssid = document.getElementById("ssid").value;
+        let password = document.getElementById("password").value;
+
+        //fetch request parsing the ssid and password data
+        fetch("/save?ssid=${ssid}&password=${password}")
+          .then(res => res.text())
+          .then(data => {
+            document.body.innerHTML = data;
+          });
+      }
+    </script>
+</body>
+</html>
+)===";
 
 void handleRoot(){
   // serve web page
-
+  server.send(200, "text/html", htmlPage);
 }
 
 void handleSaved(){
   //when ssid and pass are saved
+  
+  //recieve the data that was sent from html file
+  ssid = server.arg("ssid");
+  password = server.arg("password");
+
+  //save data to prefrences
+  prefrences.begin("wifi", true);
+  ssid =  prefrences.getString("ssid", ssid);
+  password =  prefrences.getString("password", password);
+  prefrences.end();
+
+  server.send(200, "text/html", "You have saved a network!");
+
+  shouldRestart = true;
+
 }
 
+void handleNotFound(){
+  //if a route isnt / or /save, send it to (/)
+  server.sendHeader("Location", "/");
+  server.send(302, "text/plain", "redirect tp captive portal");
+}
 
 
 void APMode(){
@@ -32,11 +93,13 @@ void APMode(){
   //starts wifi with 
   WiFi.softAP("FloridiumSetup");
   
-  //
+  //start the dns server 
   dnsServer.start(DNS_PORT, "*", WiFi.softAPIP());
 
-  server.on("/", root);
-  server.on("save", Saved);
+  server.on("/", handleRoot);
+  server.on("/save", handleSaved);
+
+  server.onNotFound(handleNotFound);
 
   server.begin();
 
@@ -45,12 +108,41 @@ void APMode(){
   // Serial.print(WiFi.softAPIP());
 }
 
+
+void POSTTaskcode(void * parameter){
+  for(;;){ //infinite loop 
+    if(WiFi.status() == WL_CONNECTED){
+      postRequest();
+
+    }
+    else{
+      Serial.print("Can't connect to the network! POST request not sent")
+      // prefrences.begin("wifi", true);
+      // numberOfAttemptsToConnect =  prefrences.getString("numberOfAttemptsToConnect", ssid);
+      // prefrences.end();
+
+      // if (numberOfAttemptsToConnect > 3){
+      //   APMode();
+      // }
+    }
+
+    //delay for an hour
+    vTaskDelay(3600000 )
+  }
+}
+
+
 void setup(){
+  Serial.begin(115200);
+
+  prefrences.begin("wifi", false);  // must be RW mode
+  prefrences.remove(numberOfAttemptsToConnect)
+  prefrences.end();
 
   //load / set up prefrences = uses "" ONLY if empty already
-  prefrences.begin("wifi, true");
-  ssid =  prefrences.getString("SSID", "");
-  password =  prefrences.getString("SSID", "");
+  prefrences.begin("wifi", true);
+  ssid =  prefrences.getString("ssid", "");
+  password =  prefrences.getString("password", "");
   prefrences.end();
 
   Serial.print("current SSID saved: ");
@@ -63,11 +155,49 @@ void setup(){
     return;
   }
 
+  WiFi.begin(ssid, password);
 
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 20){
+    delay(500);
+    Serial.print(".")#
+    attempts++;
+  }
 
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.print("Connected to network!")
+
+    //create a task to run on core 0
+    xTaskCreatePinnedToCore(
+      POSTTaskcode,     
+      "POSTTask",   
+       10000,         
+      NULL,
+      1,            
+      NULL,
+      0             
+    );
+  
+
+  } 
+  else{
+    Serial.print("Not connected to network")
+    APMode();
+  }
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (ssid == "") {
+    dnsServer.processNextRequest();
+    server.handleClient();
+    return;
+  }
+
+  //restart esp as a network has been saved
+  if(shouldRestart){
+    ESP.restart();
+  }
 
 }
