cee6d0ac7373af65e7b1bfbd9c9a86cca30c3c8f
oxxeys <147168632+oxxeys@users.noreply.github.com>
2026-04-14 21:34:14 +0100

    Temporary test for other project

---
+++ PhysicalCode/esp32code/app/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+
+
+const mongoose = require("mongoose");
+mongoose.connect("+srv://kiran1104_db_user:VsxQ2SCnphgCEGVY@floridium.vkjfn7c.mongodb.net/floridium?retryWrites=true&w=majority&appName=test")
+  .then(() => {
+    console.log("Connected to the DB");
+  })
+  .catch(err => {
+    console.log("Can't find the database!", err)
+    process.exit();
+  })
+
+const plantDataSchema = new mongoose.Schema({
+  box_id: String,
+  date_start: String,
+  temprature_reading: String,
+  moisture_reading: String,
+  sunlight_reading: String,
+  reading_date: String,
+  reading_time: { type: Date, default: Date.now }
+})
+
+const data = mongoose.model("data", plantDataSchema);
+
+app.post("/data", async (req, res) => {
+  try{
+    console.log(req.body);
+    const newData = new DataTransfer(req.body);
+    await newData.save();
+    res.send("Saved To DB");
+  }
+  catch (err){
+    console.error(err);
+    res.status(500).send("Error saving data");
+  }
+
+
+  
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
